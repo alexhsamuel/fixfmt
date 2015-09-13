69f4182083927e434a9a409a2962fac42e56a68b
Alex Samuel <alex@alexsamuel.net>
2015-09-13 16:43:46 -0400

    Reformat type object.

--- python/Number.cc
+++ python/Number.cc
@@ -60,54 +60,54 @@ PyMethodDef const tp_methods[] = {
 
 Type Number::type_ = PyTypeObject{
   PyVarObject_HEAD_INIT(nullptr, 0)
-  (char const*) "fixfmt.Number",                            // tp_name
-  (Py_ssize_t) sizeof(Number),                              // tp_basicsize
-  (Py_ssize_t) 0,                                           // tp_itemsize
-  (destructor) nullptr,                                     // tp_dealloc
-  (printfunc) nullptr,                                      // tp_print
-  (getattrfunc) nullptr,                                    // tp_getattr
-  (setattrfunc) nullptr,                                    // tp_setattr
-  (void*) nullptr,                                          // tp_reserved
-  (reprfunc) nullptr,                                       // tp_repr
-  (PyNumberMethods*) nullptr,                               // tp_as_number
-  (PySequenceMethods*) nullptr,                             // tp_as_sequence
-  (PyMappingMethods*) nullptr,                              // tp_as_mapping
-  (hashfunc) nullptr,                                       // tp_hash
-  (ternaryfunc) tp_call,                                    // tp_call
-  (reprfunc) nullptr,                                       // tp_str
-  (getattrofunc) nullptr,                                   // tp_getattro
-  (setattrofunc) nullptr,                                   // tp_setattro
-  (PyBufferProcs*) nullptr,                                 // tp_as_buffer
-  (unsigned long) Py_TPFLAGS_DEFAULT
-  | Py_TPFLAGS_BASETYPE,                                    // tp_flags
-  (char const*) nullptr,                                    // tp_doc
-  (traverseproc) nullptr,                                   // tp_traverse
-  (inquiry) nullptr,                                        // tp_clear
-  (richcmpfunc) nullptr,                                    // tp_richcompare
-  (Py_ssize_t) 0,                                           // tp_weaklistoffset
-  (getiterfunc) nullptr,                                    // tp_iter
-  (iternextfunc) nullptr,                                   // tp_iternext
-  (PyMethodDef*) tp_methods,                                // tp_methods
-  (PyMemberDef*) nullptr,                                   // tp_members
-  (PyGetSetDef*) nullptr,                                   // tp_getset
-  (_typeobject*) nullptr,                                   // tp_base
-  (PyObject*) nullptr,                                      // tp_dict
-  (descrgetfunc) nullptr,                                   // tp_descr_get
-  (descrsetfunc) nullptr,                                   // tp_descr_set
-  (Py_ssize_t) 0,                                           // tp_dictoffset
-  (initproc) tp_init,                                       // tp_init
-  (allocfunc) nullptr,                                      // tp_alloc
-  (newfunc) PyType_GenericNew,                              // tp_new
-  (freefunc) nullptr,                                       // tp_free
-  (inquiry) nullptr,                                        // tp_is_gc
-  (PyObject*) nullptr,                                      // tp_bases
-  (PyObject*) nullptr,                                      // tp_mro
-  (PyObject*) nullptr,                                      // tp_cache
-  (PyObject*) nullptr,                                      // tp_subclasses,
-  (PyObject*) nullptr,                                      // tp_weaklist
-  (destructor) nullptr,                                     // tp_del
-  (unsigned int) 0,                                         // tp_version_tag
-  (destructor) nullptr,                                     // tp_finalize
+  (char const*)         "fixfmt.Number",                    // tp_name
+  (Py_ssize_t)          sizeof(Number),                     // tp_basicsize
+  (Py_ssize_t)          0,                                  // tp_itemsize
+  (destructor)          nullptr,                            // tp_dealloc
+  (printfunc)           nullptr,                            // tp_print
+  (getattrfunc)         nullptr,                            // tp_getattr
+  (setattrfunc)         nullptr,                            // tp_setattr
+  (void*)               nullptr,                            // tp_reserved
+  (reprfunc)            nullptr,                            // tp_repr
+  (PyNumberMethods*)    nullptr,                            // tp_as_number
+  (PySequenceMethods*)  nullptr,                            // tp_as_sequence
+  (PyMappingMethods*)   nullptr,                            // tp_as_mapping
+  (hashfunc)            nullptr,                            // tp_hash
+  (ternaryfunc)         tp_call,                            // tp_call
+  (reprfunc)            nullptr,                            // tp_str
+  (getattrofunc)        nullptr,                            // tp_getattro
+  (setattrofunc)        nullptr,                            // tp_setattro
+  (PyBufferProcs*)      nullptr,                            // tp_as_buffer
+  (unsigned long)       Py_TPFLAGS_DEFAULT
+                        | Py_TPFLAGS_BASETYPE,              // tp_flags
+  (char const*)         nullptr,                            // tp_doc
+  (traverseproc)        nullptr,                            // tp_traverse
+  (inquiry)             nullptr,                            // tp_clear
+  (richcmpfunc)         nullptr,                            // tp_richcompare
+  (Py_ssize_t)          0,                                  // tp_weaklistoffset
+  (getiterfunc)         nullptr,                            // tp_iter
+  (iternextfunc)        nullptr,                            // tp_iternext
+  (PyMethodDef*)        tp_methods,                         // tp_methods
+  (PyMemberDef*)        nullptr,                            // tp_members
+  (PyGetSetDef*)        nullptr,                            // tp_getset
+  (_typeobject*)        nullptr,                            // tp_base
+  (PyObject*)           nullptr,                            // tp_dict
+  (descrgetfunc)        nullptr,                            // tp_descr_get
+  (descrsetfunc)        nullptr,                            // tp_descr_set
+  (Py_ssize_t)          0,                                  // tp_dictoffset
+  (initproc)            tp_init,                            // tp_init
+  (allocfunc)           nullptr,                            // tp_alloc
+  (newfunc)             PyType_GenericNew,                  // tp_new
+  (freefunc)            nullptr,                            // tp_free
+  (inquiry)             nullptr,                            // tp_is_gc
+  (PyObject*)           nullptr,                            // tp_bases
+  (PyObject*)           nullptr,                            // tp_mro
+  (PyObject*)           nullptr,                            // tp_cache
+  (PyObject*)           nullptr,                            // tp_subclasses,
+  (PyObject*)           nullptr,                            // tp_weaklist
+  (destructor)          nullptr,                            // tp_del
+  (unsigned int)        0,                                  // tp_version_tag
+  (destructor)          nullptr,                            // tp_finalize
 };
 
 
