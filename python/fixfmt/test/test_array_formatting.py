d1ac1dc631730c7d13c286fbacea9d628e382fe4
gwg <ggundersen@gmail.com>
2016-05-01 17:43:03 -0400

    Unit tests for array formatting.

--- python/fixfmt/test/test_array_formatting.py
+++ python/fixfmt/test/test_array_formatting.py
@@ -9,10 +9,13 @@ from   fixfmt import *
 # Mocks
 
 @pytest.fixture
-def patch_shutil_get_terminal_size(monkeypatch):
+def mock_terminal_size(monkeypatch):
+    """
+    Mock terminal size to make tests independent of actual terminal size.
+    """
 
     class TerminalSize:
-        columns = 90
+        columns = 50
 
     def mock():
         return TerminalSize()
@@ -22,11 +25,79 @@ def patch_shutil_get_terminal_size(monkeypatch):
 #-------------------------------------------------------------------------------
 # Tests
 
-def test_shutil_mock(patch_shutil_get_terminal_size):
+def test_shutil_mock(mock_terminal_size):
     pass
 
 
-def test_vertical_formatter():
-    fmt  = NdArray(Number(4, 4), axis=1)
-    arr  = np.array([8, math.pi, 4.88])
-    assert fmt(arr) == "    8.0000\n    3.1416\n    4.8800"
+def test_1d_array(mock_terminal_size):
+    fmt = Array(Number(3, 2))
+    arr = np.array([8, math.pi, 4.88])
+    assert fmt(arr) == "[   8.00,    3.14,    4.88]"
+    fmt = Array(String(6))
+    arr = np.array(["Aleksandrov", "Kolmogorov", "Lavrent'ev"])
+    assert fmt(arr) == "[Aleks…, Kolmo…, Lavre…]"
+
+
+def test_vertical_orientation(mock_terminal_size):
+    fmt = Array(Number(3, 2), axis=1)
+    arr = np.array([8, math.pi, 4.88])
+    assert fmt(arr) == "[   8.00\n.   3.14\n.   4.88]"
+
+
+def test_dimension_separator(mock_terminal_size):
+    fmt = Array(Number(2), dim_sep=';')
+    arr = np.array([[1,2,3], [4,55,6]])
+    assert fmt(arr) == "[[  1,   2,   3];\n.[  4,  55,   6]]"
+
+
+def test_element_separator(mock_terminal_size):
+    fmt = Array(Number(3, 2), elem_sep='|')
+    arr = np.array([8, math.pi, 4.88])
+    assert fmt(arr) == "[   8.00|   3.14|   4.88]"
+
+
+def test_element_separator(mock_terminal_size):
+    fmt = Array(Number(3, 2), elem_sep='|')
+    arr = np.array([8, math.pi, 4.88])
+    assert fmt(arr) == "[   8.00|   3.14|   4.88]"
+
+
+def test_wrap_1d_array(mock_terminal_size):
+    fmt = Array(Number(3, 2))
+    arr = np.zeros(20)
+    assert fmt(arr) == ""\
+        "[   0.00,    0.00,    0.00,    0.00,    0.00, \n"\
+        ".   0.00,    0.00,    0.00,    0.00,    0.00, \n"\
+        ".   0.00,    0.00,    0.00,    0.00,    0.00, \n"\
+        ".   0.00,    0.00,    0.00,    0.00,    0.00]"
+
+
+def test_wrap_3d_array(mock_terminal_size):
+    fmt = Array(Number(3, 2))
+    arr = np.zeros((3,2,20))
+    assert fmt(arr) == ""\
+        "[[[   0.00,    0.00,    0.00,    0.00,    0.00, \n"\
+        "...   0.00,    0.00,    0.00,    0.00,    0.00, \n"\
+        "...   0.00,    0.00,    0.00,    0.00,    0.00, \n"\
+        "...   0.00,    0.00,    0.00,    0.00,    0.00],\n"\
+        "..[   0.00,    0.00,    0.00,    0.00,    0.00, \n"\
+        "...   0.00,    0.00,    0.00,    0.00,    0.00, \n"\
+        "...   0.00,    0.00,    0.00,    0.00,    0.00, \n"\
+        "...   0.00,    0.00,    0.00,    0.00,    0.00]],\n"\
+        ".[[   0.00,    0.00,    0.00,    0.00,    0.00, \n"\
+        "...   0.00,    0.00,    0.00,    0.00,    0.00, \n"\
+        "...   0.00,    0.00,    0.00,    0.00,    0.00, \n"\
+        "...   0.00,    0.00,    0.00,    0.00,    0.00],\n"\
+        "..[   0.00,    0.00,    0.00,    0.00,    0.00, \n"\
+        "...   0.00,    0.00,    0.00,    0.00,    0.00, \n"\
+        "...   0.00,    0.00,    0.00,    0.00,    0.00, \n"\
+        "...   0.00,    0.00,    0.00,    0.00,    0.00]],\n"\
+        ".[[   0.00,    0.00,    0.00,    0.00,    0.00, \n"\
+        "...   0.00,    0.00,    0.00,    0.00,    0.00, \n"\
+        "...   0.00,    0.00,    0.00,    0.00,    0.00, \n"\
+        "...   0.00,    0.00,    0.00,    0.00,    0.00],\n"\
+        "..[   0.00,    0.00,    0.00,    0.00,    0.00, \n"\
+        "...   0.00,    0.00,    0.00,    0.00,    0.00, \n"\
+        "...   0.00,    0.00,    0.00,    0.00,    0.00, \n"\
+        "...   0.00,    0.00,    0.00,    0.00,    0.00]]]"
+    
