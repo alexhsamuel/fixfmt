69369fce3bd57162e07798b9f8e90acb6e2b424d
Alex Samuel <alex@alexsamuel.net>
2017-01-03 08:31:36 -0500

    Syntax for Python 2.7.

--- python/fixfmt/numpy.py
+++ python/fixfmt/numpy.py
@@ -2,7 +2,7 @@ from   __future__ import absolute_import, division, print_function
 
 class Array:
 
-    def __init__(self, fmt, width=None, *, left="", right="", margin=" "):
+    def __init__(self, fmt, width=None, left="", right="", margin=" "):
         self.__fmt = fmt
         self.__width = width
         self.__left = left
